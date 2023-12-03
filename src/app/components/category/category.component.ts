b57269d03a2892f0f8e8218272d3004c60d9cd33
tony <tonykcho3527657@gmail.com>
2023-12-03 15:08:42 -0500

    Category style

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { EventComponent } from '../event/event.component';
 
 @Component({
   selector: 'app-category',
   standalone: true,
-  imports: [],
+  imports: [EventComponent],
   templateUrl: './category.component.html',
   styleUrl: './category.component.scss'
 })
