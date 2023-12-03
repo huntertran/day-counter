fc0682430d00f38357175a60c8f4a5d02cb5bdc5
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2023-12-03 15:42:53 -0500

    storage service completed

--- src/app/services/storage.service.ts
+++ src/app/services/storage.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import store from 'store2';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +7,25 @@ import { Injectable } from '@angular/core';
 export class StorageService {
 
   constructor() { }
+
+  public create(main: Main): void {
+    store.set(main.User, main.Categories, true);
+  }
+
+  public read(user: string): Main {
+    let main: Main = {
+      User: user,
+      Categories: store.get(user, [])
+    }
+
+    return main;
+  }
+
+  public update(main: Main): void {
+    store.set(main.User, main.Categories);
+  }
+
+  public delete(): void {
+    store.clear();
+  }
 }
