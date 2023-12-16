825687e81d3a78924c055f78e7a833b1b6e78fa5
Tuan Tran Van <4468525+huntertran@users.noreply.github.com>
2023-12-16 16:08:23 -0500

    login with google

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,30 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getAnalytics, provideAnalytics, ScreenTrackingService, UserTrackingService } from '@angular/fire/analytics';
+import { initializeAppCheck, ReCaptchaEnterpriseProvider, provideAppCheck } from '@angular/fire/app-check';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+    providers: [
+        provideRouter(routes),
+        provideAnimations(),
+        importProvidersFrom(provideFirebaseApp(() => initializeApp({
+            "projectId": "hunter-day-counter",
+            "appId": "1:102441183745:web:56c19bc2f3beac8dd92e00",
+            "storageBucket": "hunter-day-counter.appspot.com",
+            "apiKey": "AIzaSyB8VKMvfOYrISdce2tDWbbiHdFgOcx26rA",
+            "authDomain": "hunter-day-counter.firebaseapp.com",
+            "messagingSenderId": "102441183745",
+            "measurementId": "G-PB00HLR9EL"
+        }))),
+        importProvidersFrom(provideAuth(() => getAuth())),
+        importProvidersFrom(provideAnalytics(() => getAnalytics())),
+        ScreenTrackingService,
+        UserTrackingService,
+        importProvidersFrom(provideFirestore(() => getFirestore()))
+    ]
 };
--- src/app/components/user-settings/user-settings.component.ts
+++ src/app/components/user-settings/user-settings.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Auth, signInWithPopup, GoogleAuthProvider, getAuth } from '@angular/fire/auth';
 import { FormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import {
@@ -18,5 +19,29 @@ import { MatInputModule } from '@angular/material/input';
     styleUrl: './user-settings.component.scss'
 })
 export class UserSettingsComponent {
+    private auth: Auth = inject(Auth);
 
+    public loginWithGoogle(): void {
+        const provider = new GoogleAuthProvider();
+        const auth = getAuth();
+        signInWithPopup(auth, provider)
+            .then((result) => {
+                // This gives you a Google Access Token. You can use it to access the Google API.
+                const credential = GoogleAuthProvider.credentialFromResult(result);
+                const token = credential?.accessToken;
+                // The signed-in user info.
+                const user = result.user;
+                // IdP data available using getAdditionalUserInfo(result)
+                // ...
+            }).catch((error) => {
+                // Handle Errors here.
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // The email of the user's account used.
+                const email = error.customData.email;
+                // The AuthCredential type that was used.
+                const credential = GoogleAuthProvider.credentialFromError(error);
+                // ...
+            });
+    }
 }
